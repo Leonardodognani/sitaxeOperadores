2df1f572229f60735872fef987056ad9bc301e66
Leonardodognani <82122343+Leonardodognani@users.noreply.github.com>
2021-10-12 16:16:20 -0300

    first commit, add function

---
+++ index.js
@@ -0,0 +1,29 @@
+function compareNumbers (num1, num2) {
+
+    if(num1 === num2 && num1 + num2 < 10){
+        console.log(`Os números ${num1} e ${num2} são iguais. Sua soma é ${num1 + num2}, a qual é menor que 10 e, portanto, menor que 20.`);
+    } else if(num1 === num2 && num1 + num2 > 10 && num1 + num2 < 20) {
+        console.log(`Os números ${num1} e ${num2} são iguais. Sua soma é ${num1 + num2}, a qual é maior do que 10 e menor do que 20.`);
+    } else if(num1 === num2 && num1 + num2 > 20){
+        console.log(`Os números ${num1} e ${num2} são iguais. Sua soma é ${num1 + num2}, a qual é maior do que 20 e, portanto, maior do que 10.`);
+    } else if(num1 === num2 && num1 + num2 === 10){
+        console.log(`Os números ${num1} e ${num2} são iguais. Sua soma é ${num1 + num2}, a qual é menor do que 20.`);
+    } else if(num1 === num2 && num1 + num2 === 20){
+        console.log(`Os números ${num1} e ${num2} são iguais. Sua soma é ${num1 + num2}, a qual é maior do que 10.`);
+    } else if(num1 !== num2 && num1 + num2 < 10){
+        console.log(`Os números ${num1} e ${num2} não são iguais. Sua soma é ${num1 + num2}, a qual é menor que 10 e, portanto, menor que 20.`);
+    } else if(num1 !== num2 && num1 + num2 > 10 && num1 + num2 < 20) {
+        console.log(`Os números ${num1} e ${num2} não são iguais. Sua soma é ${num1 + num2}, a qual é maior do que 10 e menor do que 20.`);
+    } else if(num1 !== num2 && num1 + num2 > 20){
+        console.log(`Os números ${num1} e ${num2} não são iguais. Sua soma é ${num1 + num2}, a qual é maior do que 20 e, portanto, maior do que 10.`);
+    } else if(num1 !== num2 && num1 + num2 === 10){
+        console.log(`Os números ${num1} e ${num2} não são iguais. Sua soma é ${num1 + num2}, a qual é menor do que 20.`);
+    } else if(num1 !== num2 && num1 + num2 === 20){
+        console.log(`Os números ${num1} e ${num2} não são iguais. Sua soma é ${num1 + num2}, a qual é maior do que 10.`);
+    }
+
+}
+    
+
+compareNumbers(5, 6);
+
